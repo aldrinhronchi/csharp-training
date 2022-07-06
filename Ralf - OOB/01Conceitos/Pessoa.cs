057b9c8ef2900f192a7a04d571569754c407eb6e
Terore <aldrinhronchi@gmail.com>
2022-07-06 13:19:48 -0300

    Alguns exercícios feitos do tutorial para pratica de C#
    Alguns exercícios feitos do tutorial para pratica de C#

---
+++ Ralf - OOB/01Conceitos/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+namespace _01Conceitos {
+class Pessoa
+{
+    public string nome;
+    public int idade;
+
+    public void mensagem()
+    {
+        Console.WriteLine("Ola "+nome+" voce tem "+idade+"anos");
+    }
+}}
