021eac908b2cf05bd4b6d162451be680d3b6487a
Terore <aldrinhronchi@gmail.com>
2023-02-08 21:49:50 -0300

    Aula 12

---
+++ Ralf - OOB/12Estatico/Exemplo.cs
@@ -0,0 +1,13 @@
+using System;
+namespace _12Estatico
+{
+    class Exemplo
+    {
+        public static String nome = "Ralf";
+        // Metodo estatico
+        public static void soma(int n1, int n2)
+        {
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
---
+++ Ralf - OOB/12Estatico/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace _12Estatico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Exemplo.soma(3, 7);
+
+            Console.WriteLine("Ola " + Exemplo.nome);
+        }
+    }
+}
